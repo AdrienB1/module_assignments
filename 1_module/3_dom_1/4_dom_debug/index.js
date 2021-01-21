87269a71e9636fed3e29151f9b58d628f0053e92
Adrien Burr <adrienburr@gmail.com>
2021-01-21 00:43:27 -0700

    Fixed a few bugs within this code, dont remember all of them, but the list item now generates when you click add with the input text you have entered, the drop down shows all colors from the 'colors' array, and when you select a color the drop down changes accordingly. Also, the drop down starts white. I still need to set it so that the list item rather than the drop down has the background color, but will fix later.

---
+++ 1_module/3_dom_1/4_dom_debug/index.js
@@ -0,0 +1,32 @@
+const colors = ["red", "blue", "green"]
+
+document.getElementById("add").addEventListener("click", function(e){
+    const subItem = createSubItem(e)
+    document.getElementById("list").appendChild(subItem)
+})
+
+function createDropDown(){
+    const dropDown = document.createElement("select")
+    for (let i = 0; i < colors.length; i++){
+        const option = document.createElement("option") 
+        option.innerHTML = colors[i]
+        option.value = colors[i]
+        dropDown.append(option)
+    }
+    dropDown.addEventListener("input", function(e){
+        e.target.style.backgroundColor = e.target.value
+    })
+    return dropDown
+}
+
+function createSubItem(e){
+    const subItem = document.createElement("div")
+    var subItemValue = document.getElementById("input").value
+    subItem.textContent = subItemValue
+    const dropDown = createDropDown()
+    subItem.appendChild(dropDown)
+    subItem.setAttribute("class", "subItem")
+    return subItem
+}
+
+
