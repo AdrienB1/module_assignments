0f4782dcabd87f3320255fae1eaddcc3dba35b44
Adrien Burr <adrienburr@gmail.com>
2021-02-27 09:07:37 -0700

    added to the 'loop olympics' assignmrnt

---
+++ 1_module/4_js_1/1_loop_olympics/main.js
@@ -0,0 +1,21 @@
+//Write a for loop that prints to the console the numbers 0 through 9.
+
+for (i = 0; i <= 9; i++) {
+    console.log(i)
+}
+
+
+
+//Write a for loop that prints to the console 9 through 0.
+
+
+
+
+
+//Write a for loop that prints these fruits to the console.
+
+
+
+
+
+//var fruit = ["banana", "orange", "apple", "kiwi"]
