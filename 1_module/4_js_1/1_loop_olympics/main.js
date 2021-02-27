aa6aa87d492b25351a0b4d15bb0eda949ba87e9d
Adrien Burr <adrienburr@gmail.com>
2021-02-27 09:11:26 -0700

    added to the 'loop olympics' assignment, finished bronze

--- 1_module/4_js_1/1_loop_olympics/main.js
+++ 1_module/4_js_1/1_loop_olympics/main.js
@@ -8,14 +8,15 @@ for (i = 0; i <= 9; i++) {
 
 //Write a for loop that prints to the console 9 through 0.
 
-
+for (i = 9; i >= 0; i--) {
+    console.log(i)
+}
 
 
 
 //Write a for loop that prints these fruits to the console.
+var fruit = ["banana", "orange", "apple", "kiwi"]
 
-
-
-
-
-//var fruit = ["banana", "orange", "apple", "kiwi"]
+for (i = 0; i < fruit.length; i++) {
+    console.log(fruit[i])
+}
