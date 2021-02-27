c43fbc5f3609f404153b0decc42937d4badcfe5b
Adrien Burr <adrienburr@gmail.com>
2021-02-27 09:27:12 -0700

    added to 'loop olympics' assignment

--- 1_module/4_js_1/1_loop_olympics/main.js
+++ 1_module/4_js_1/1_loop_olympics/main.js
@@ -1,3 +1,5 @@
+//---------------------PRELIMINARIES---------------------
+
 //Write a for loop that prints to the console the numbers 0 through 9.
 
 for (i = 0; i <= 9; i++) {
@@ -20,3 +22,47 @@ var fruit = ["banana", "orange", "apple", "kiwi"]
 for (i = 0; i < fruit.length; i++) {
     console.log(fruit[i])
 }
+
+
+
+//---------------------BRONZE---------------------
+
+//Write a for loop that will push the numbers 0 through 9 to an array.
+var nineSoon = []
+
+for (i = 0; i <= 9; i++) {
+    nineSoon.push(i)
+}
+
+console.log(nineSoon)
+
+
+
+//Write a for loop that prints to the console only even numbers 0 through 100.
+var evenNumbers = []
+
+for (i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+        evenNumbers.push(i)
+    } else {
+
+    }
+}
+
+console.log(evenNumbers)
+
+
+//Write a for loop that will push every other fruit to an array.
+var fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+
+var otherFruit = []
+
+for (i = 0; i < fruit.length; i++) {
+    if (i % 2 !== 0) {
+        otherFruit.push(fruit[i])
+    } else {
+
+    }
+}
+
+console.log(otherFruit)
