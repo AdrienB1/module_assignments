f2b6e75c44b92f5889cd47edb3168108e96833d0
Adrien Burr <adrienburr@gmail.com>
2021-03-09 15:27:14 -0700

    Set up for the 'array methods excercise'

---
+++ 1_module/4_js_1/2_array_methods_excercise/main.js
@@ -0,0 +1,54 @@
+var fruit = ["banana", "apple", "orange", "watermelon"];
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+
+
+console.log("fruit original: ", fruit);
+console.log("vegetables original: ", vegetables);
+
+
+// 1 - Remove the last item from the vegetable array.
+
+
+
+
+// 2 - Remove the first item from the fruit array.
+
+
+
+
+// 3 - Find the index of "orange."
+
+
+
+
+// 4 - Add that number to the end of the fruit array.
+
+
+
+
+// 5 - Use the length property to find the length of the vegetable array.
+
+
+
+
+// 6 - Add that number to the end of the vegetable array.
+
+
+
+
+// 7 - Put the two arrays together into one array. Fruit first. Call the new Array "food".
+
+
+
+
+// 8 - Remove 2 elements from your new array starting at index 4 with one method.
+
+
+
+
+// 9 - Reverse your array.
+
+
+
+
+// 10 - Turn the array into a string and return it.
