7e581a3ccb369b87b5bb26bc703e8cec791e2e40
Adrien Burr <adrienburr@gmail.com>
2021-03-09 16:45:45 -0700

    Finished 'Forception' assignment

---
+++ 1_module/4_js_1/3_forception/main.js
@@ -0,0 +1,38 @@
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+var alphabet = alphabet.split("")
+
+
+var runIt = forception(people, alphabet)
+
+
+console.log(runIt)
+
+
+
+
+
+function forception(people, alphabet){
+    // my code here
+    var outputArray = []
+    for (var i = 0; i < people.length; i++) {
+        outputArray.push(people[i])
+        for (var c = 0; c < alphabet.length; c++) {
+            outputArray.push(alphabet[c])
+        }
+    }
+    return outputArray
+}
+
+
+
+/* Output: 
+[
+"Jon:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
+"Jacob:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
+"Jingle:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
+"Heimer:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
+"Schmidt:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+]
+*/
