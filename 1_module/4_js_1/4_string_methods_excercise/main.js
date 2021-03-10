d380df30b1d0ee518cce74be9fdd405ccecb4c7f
Adrien Burr <adrienburr@gmail.com>
2021-03-10 12:37:10 -0700

    Set up for the 'String Methods Excercise' assignment

---
+++ 1_module/4_js_1/4_string_methods_excercise/main.js
@@ -0,0 +1,25 @@
+// 1 - Write a function that takes a string as a parameter and returns the same string in all capital letters followed by all lowercase letters.
+var stringOne = "MoBIuS"
+function upperLower(stringOne) {
+    
+}
+
+
+// 2 - Write a function that takes a string as a parameter and returns a number that is half the string's length, rounded down.
+//Hint: You'll need to use Math.floor().
+
+
+
+// 3 - Write a function that uses slice() and the other functions you've written to return the first half of the given string.
+
+
+
+// 4 - Write a function that takes a string as a parameter and returns that string where the first half is capitalized, and the second half is lowercase.
+//Hint: If your string length is odd, use Math.floor() to round down.
+
+
+
+// 5 - Write a function that takes a string as a parameter and capitalizes any character that follows a space.
+//(This one is a step up in difficulty and utilizes the .split() string method and .join() array method):
+
+
