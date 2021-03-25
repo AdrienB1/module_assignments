7269bd55d9600e14f38d988b46339cbc41f1d0fb
Adrien Burr <adrienburr@gmail.com>
2021-03-24 21:25:00 -0600

    added some responsive styling to 'travel form' assignment

--- 2_module/0_js_2/1_travel_form/main.js
+++ 2_module/0_js_2/1_travel_form/main.js
@@ -1,11 +1,52 @@
 //---------------Variables---------------
-
 const travelForm = document.getElementById("myForm")
 
 //---------------Variables---------------
 travelForm.addEventListener("submit", endAlert)
 
+//---------------Styling---------------
+const textInputs = document.querySelectorAll(".inputBox")
+
+textInputs[0].addEventListener("focus", edgeGlow)
+textInputs[0].addEventListener("blur", removeEdgeGlow)
+
+textInputs[1].addEventListener("focus", edgeGlow)
+textInputs[1].addEventListener("blur", removeEdgeGlow)
+
+textInputs[2].addEventListener("focus", edgeGlow)
+textInputs[2].addEventListener("blur", removeEdgeGlow)
+
+travelForm.location.addEventListener("focus", edgeGlow)
+travelForm.location.addEventListener("blur", function() {
+    travelForm.location.style.boxShadow = "none"
+})
+
+travelForm.submitButton.addEventListener("mouseover", function() {
+    travelForm.submitButton.style.backgroundColor = "#F2B885"
+    travelForm.submitButton.style.color = "black"
+})
+travelForm.submitButton.addEventListener("mouseout", function() {
+    travelForm.submitButton.style.backgroundColor = "#35478C"
+    travelForm.submitButton.style.color = "white"
+})
+travelForm.submitButton.addEventListener("mousedown", function() {
+    travelForm.submitButton.style.backgroundColor = "#F2937E"
+})
+travelForm.submitButton.addEventListener("mouseup", function() {
+    travelForm.submitButton.style.backgroundColor = "#F2B885"
+})
+
 //---------------Functions---------------
+
+function edgeGlow() {
+    document.activeElement.style.boxShadow = "0 0 4px 1px #F2CD88"
+}
+
+function removeEdgeGlow() {
+    for(i = 0; i < textInputs.length; i++) {
+        textInputs[i].style.boxShadow = "none"
+    }
+}
 
 function endAlert() {
     let fName = travelForm.firstName.value
