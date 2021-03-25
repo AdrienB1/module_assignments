3e355404d41c645d766543a829ac974043d92440
Adrien Burr <adrienburr@gmail.com>
2021-03-24 20:57:27 -0600

    Completed JS for 'travel form' assignment

--- 2_module/0_js_2/1_travel_form/main.js
+++ 2_module/0_js_2/1_travel_form/main.js
@@ -1,20 +1,39 @@
-
 //---------------Variables---------------
 
 const travelForm = document.getElementById("myForm")
 
-
 //---------------Variables---------------
 travelForm.addEventListener("submit", endAlert)
 
-
 //---------------Functions---------------
 
-function endAlert(event) {
-    event.preventDefault()
+function endAlert() {
     let fName = travelForm.firstName.value
     let lName = travelForm.lastName.value
     let age = travelForm.age.value
+    let gender = travelForm.gender.value
+    let location = travelForm.location.value
+    let dietArray = travelForm.diet
+    let dietChoices = []
 
-    alert("First Name: " + fName "\nLast Name: " + lName "\nAge: " + age)
+    for (i = 0; i < dietArray.length; i++) {
+        if (dietArray[i].checked) {
+            dietChoices.push(dietArray[i].value)
+        }
+    }
+
+    alert(
+        "First Name: " +
+            fName +
+            "\nLast Name: " +
+            lName +
+            "\nAge: " +
+            age +
+            "\nGender: " +
+            gender +
+            "\nDestination: " +
+            location +
+            "\nDiet:" +
+            dietChoices
+    )
 }
