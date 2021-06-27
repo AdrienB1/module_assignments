79e9dd0448f2bed20a9b6cc44a4c1d9702d07273
Adrien Burr <adrienburr@gmail.com>
2021-06-27 03:54:45 -0600

    forgot to reformat the code after poking through it, fixed

--- 2_module/0_js_2/3_go_speed_clicker/main.js
+++ 2_module/0_js_2/3_go_speed_clicker/main.js
@@ -9,7 +9,6 @@ if (isNaN(Number(savedClicks)) == false) {
     clickCountDisplay.innerHTML = sessionStorage.getItem(savedClicks)
 }
 
-
 //Check whether the content of the click count display is a number,
 //if it is, set the font color back to white
 if (isNaN(Number(document.querySelector(".clickCountSpan").innerHTML))) {
@@ -26,11 +25,6 @@ let hasClicked = false
 clickSpace.addEventListener("click", countClick)
 
 //using localStorage or sessionStorage, make it so the number of clicks will remain on the screen even after the site is refreshed
-
-
-
-
-
 
 //Extra credit: Put a 30 sec. countdown timer on the page that stops clicks from counting after the timer ends
 clickSpace.addEventListener("mousedown", timeStart)
@@ -51,7 +45,7 @@ function countClick() {
     }
 
     //slice countdown to grab just the number (or a number and a space)
-    let currentTime = Number(timerDisplay.innerHTML.slice(0,2))
+    let currentTime = Number(timerDisplay.innerHTML.slice(0, 2))
 
     //Check whether current time displayed in the timer is above 0
     //if it is, add the most reccent click to trigger this function
@@ -61,7 +55,6 @@ function countClick() {
         //save new click sum to session storage
         sessionStorage.setItem(savedClicks, clickCountNew)
     }
-
 
     //Check whether the content of the click count display is a number,
     //if it is, set the font color to white, if not, do nothing
@@ -96,9 +89,9 @@ function timeStart() {
             if (time > 0) {
                 time = Number(time) - 1
                 timerDisplay.innerHTML = time + unit
-                    if (time == 0) {
-                        timerDisplay.style.color = "#D40D12"
-                    }
+                if (time == 0) {
+                    timerDisplay.style.color = "#D40D12"
+                }
             } else {
                 stopTimer()
                 timerDisplay.innerHTML = "Time's Up!"
