d633b931e5cec9c7a582c3862fa111974466286f
Adrien Burr <adrienburr@gmail.com>
2022-08-01 15:30:27 -0600

    finished all of question 1

--- 2_module/1_testing/0_catch_me_if_you_can/main.js
+++ 2_module/1_testing/0_catch_me_if_you_can/main.js
@@ -1,14 +1,22 @@
 // 1a) Write a function that returns the sum of two numbers. Throw an error if either argument is not of the data type number:
 function sum(x, y) {
     //check data types first and throw error
-    if ()
+    if (isNaN(x)) {
+        throw new Error(x + " is not a number!")
+    } else if (isNaN(y)) {
+        throw new Error(y + " is not a number!")
+    }
     return x + y
 }
 
-
 // 1b) Call the sum function inside a try block using "1" and "2" as arguments.
+try {
+    console.log(sum(1, 2))
+}
 // Use console.log within a catch block to inform the user of the error.
-
+catch(err) {
+    console.log(err)
+}
 
 
 //<><><><><><><><><><><><><><><><><> Question 2 below <><><><><><><><><><><><><><><><><><><></>
